ad519baa53f5a399d276b42f5b865fdf38a804c5
CocoCocoAlx <vania182004@gmail.com>
2022-07-24 10:07:09 -0400

    Implementación JWT I
    Creación de métodos de validación.

---
+++ src/main/java/com/ivanwportfolio/arprog/Seguridad/JWT/jwtFiltroToken.java
@@ -0,0 +1,49 @@
+package com.ivanwportfolio.arprog.Seguridad.JWT;
+
+import com.ivanwportfolio.arprog.Seguridad.Servicio.ImpUsuarioDetalles;
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class jwtFiltroToken extends OncePerRequestFilter {
+
+    private final static Logger logger = LoggerFactory.getLogger(jwtProvider.class);
+
+    @Autowired
+    jwtProvider jwtProvider;
+    @Autowired
+    ImpUsuarioDetalles impUsuarioDetalles;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String token = getToken(request);
+            if (token != null && jwtProvider.ValidateToken(token)) {
+                String nombreUsuario = jwtProvider.getNombreUsuarioDeToken(token);
+                UserDetails usuarioDetalles = impUsuarioDetalles.loadUserByUsername(nombreUsuario);
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(usuarioDetalles, null, usuarioDetalles.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        } catch (Exception e) {
+            logger.error("Falló el método doFilterInternal en jwtFiltroToken");
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String getToken(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+        if (header != null && header.startsWith("Bearer")) {
+            return header.replace("Bearer", "");
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/ivanwportfolio/arprog/Seguridad/JWT/jwtProvider.java
@@ -0,0 +1,53 @@
+package com.ivanwportfolio.arprog.Seguridad.JWT;
+
+import com.ivanwportfolio.arprog.Seguridad.Entidad.Admin;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import java.util.Date;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+@Component
+public class jwtProvider {
+
+    private final static Logger logger = LoggerFactory.getLogger(jwtProvider.class);
+
+    @Value("$(jwt.secret")
+    private String secreto;
+    @Value("$(jwt.expiration")
+    private int expiration;
+
+    private String generateToken(Authentication auth) {
+        Admin admin = (Admin) auth.getPrincipal();
+        return Jwts.builder().setSubject(admin.getUsername()).setIssuedAt(new Date()).setExpiration(new Date(new Date().getTime() + expiration * 3600)).signWith(SignatureAlgorithm.HS512, secreto).compact();
+    }
+
+    public String getNombreUsuarioDeToken(String token) {
+        return Jwts.parser().setSigningKey(secreto).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean ValidateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secreto).parseClaimsJws(token);
+            return true;
+        } catch (MalformedJwtException e) {
+            logger.error("El token está mal formado");
+        } catch (ExpiredJwtException e) {
+            logger.error("El token ha expirado");
+        } catch (IllegalArgumentException e) {
+            logger.error("El token está vacío");
+        } catch (SignatureException e) {
+            logger.error("Error en la firma del token");
+        } catch (UnsupportedJwtException e) {
+            logger.error("Token no soportado");
+        }
+        return false;
+    }
+}
